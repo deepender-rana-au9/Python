c5ea7f237841f18ab53438634c2432b21230e629
Deepender Rana <chinturana97@gmail.com>
2020-12-20 22:32:03 +0530

    sandPaperSissor

--- JS/app.js
+++ JS/app.js
@@ -6,3 +6,27 @@ const upper = (text) => {
 };
 text = "abcdefghijklmnopqrstuvwxyz";
 console.log(upper(text));
+age = 23;
+name = "Deepender Rana";
+console.log(`Hello my name is ${age + name}`);
+
+//Array
+const schedule = ["Wake Up", "Eat", "Study", "Watch videos on Netflix"];
+console.log(schedule.push("Sleep"));
+//Methods
+schedule.pop();
+schedule.push("ABC");
+schedule.shift();
+schedule.unshift("XYZ");
+
+console.log(this);
+
+const obj = {
+  abc: "abv",
+  1: "123",
+  3: function () {
+    console.log(Hii);
+  },
+};
+
+console.log(obj.abc);
