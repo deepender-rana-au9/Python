658891ffd93e709150cf5d9d95af976b3c30f58b
Deepender Rana <chinturana97@gmail.com>
2020-12-21 20:16:21 +0530

    pattern

---
+++ Python/Patterns/7th.py
@@ -0,0 +1,33 @@
+'''
+Pattern:
+
+      *   
+     * *   
+    * * *   
+   * * * *   
+  * * * * *   
+ * * * * * *   
+* * * * * * *
+      
+'''
+
+
+size = 7
+
+m = (size) - 1
+
+for i in range(0, size):
+
+    for j in range(0, m):
+
+        print(end=" ")
+
+    m = m - 1  # decrementing m after each loop
+
+    for j in range(0, i + 1):
+
+        # printing full Triangle pyramid using stars
+
+        print("*", end=" ")
+
+    print(" ")
