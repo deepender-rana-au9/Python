53b2839745e0434701f5b828135c8eadf2c979eb
Deepender Rana <chinturana97@gmail.com>
2021-02-12 22:10:41 +0530

    guess game

---
+++ JS/guessTheNumber/script.js
@@ -0,0 +1,74 @@
+let guess = [];
+let correctNumber = getRandomNumber();
+
+window.onload = function () {
+  console.log(correctNumber);
+  document.getElementById("btn-submit").addEventListener("click", playgame);
+  document.getElementById("btn-restart").addEventListener("click", initGame);
+};
+
+function playgame(randomNumber) {
+  let number = document.getElementById("number-guess").value;
+  number = Number(number);
+  document.getElementById("number-guess").value = "";
+  document.getElementById("number-guess").placeholder = "type here";
+
+  saveGuessHistory(number, guess);
+  displayResult(number);
+}
+
+function displayResult(number) {
+  if (number === 0) {
+    document.getElementById("result").style.display = "block";
+    document.getElementById("result").innerText = "Enter something ";
+    document.getElementById("result").style.background = "rgb(184, 58, 58)";
+  } else if (number === correctNumber) {
+    document.getElementById("result").style.display = "block";
+    showYouWon();
+  } else if (number > correctNumber) {
+    document.getElementById("result").style.display = "block";
+    tooHigh();
+  } else {
+    document.getElementById("result").style.display = "block";
+    tooLow();
+  }
+}
+
+function showYouWon() {
+  const text = "Awesome job, you got it!";
+  document.getElementById("result").innerText = text;
+  document.getElementById("result").style.background = "rgb(72, 189, 72)";
+  counter = 0;
+}
+
+function tooHigh() {
+  const text = "Ohh! too high";
+  document.getElementById("result").innerText = text;
+  document.getElementById("result").style.background = "rgb(184, 58, 58)";
+}
+
+function tooLow() {
+  const text = "Ohh! too Low";
+  document.getElementById("result").innerText = text;
+  document.getElementById("result").style.background = "rgb(184, 58, 58)";
+}
+
+function saveGuessHistory(number, guess) {
+  guess.push(number);
+}
+
+function displayHistory() {
+  let index;
+  let list = "<ul class='list'>";
+
+  list += "</ul>";
+  document.getElementById("history").innerHTML = list;
+}
+
+function initGame() {}
+
+function getRandomNumber() {
+  let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+  return randomNumber;
+}
