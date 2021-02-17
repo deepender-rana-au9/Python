1653b439b84c21d8d3fb1f528406d84ef9271e52
Deepender Rana <chinturana97@gmail.com>
2021-02-17 14:47:34 +0530

    itertors

---
+++ JS/iterating.js
@@ -0,0 +1,26 @@
+/*
+Iterating
+*/
+
+let fruits = ["apple", "banana ", "orange", "cherries", "jachfruits", "guava"];
+
+// console.log("Simple for loop");
+
+// for (let i = 0; i < fruits.length; i++) {
+//   console.log(fruits[i]);
+// }
+
+// console.log("for-of");
+// for (let value of fruits) {
+//   console.log(value);
+// }
+
+// console.log("forEach");
+// fruits.forEach((value) => {
+//   console.log(value);
+// });
+
+console.log("map");
+fruits.map((value) => {
+  console.log(value);
+});
