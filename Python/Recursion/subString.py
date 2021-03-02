45135b81f7159e4f31a862342b56673cc7f0a8be
Deepender Rana <chinturana97@gmail.com>
2021-03-02 23:23:11 +0530

    substring

--- Python/Recursion/subString.py
+++ Python/Recursion/subString.py
@@ -1,10 +1,25 @@
 # Simple way to get sunstrings without recursion
 
-def subStrings(string):
-    for i in range(len(string)):
-        for j in range(i, len(string)):
-            print(string[i:j+1])
+# def subStrings(string):
+#     for i in range(len(string)):
+#         for j in range(i, len(string)):
+#             print(string[i:j+1])
+
+
+# string = input("Enter a string ")
+# subStrings(string)
+
+
+# With recursion
+def subStrings(string, res, idx):
+    if idx > len(string):
+        print(res)
+        return
+    subStrings(string, res + string[:idx], idx+1)
+    subStrings(string, res, idx+1)
 
 
 string = input("Enter a string ")
-subStrings(string)
+res = []
+idx = 0
+subStrings(string, res, idx)
