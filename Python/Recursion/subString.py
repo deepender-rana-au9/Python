ca50f69d3328bb5423ed553f75677e26ffc54922
Deepender Rana <chinturana97@gmail.com>
2021-03-03 23:58:41 +0530

    Favonacci

--- Python/Recursion/subString.py
+++ Python/Recursion/subString.py
@@ -1,6 +1,7 @@
-# Simple way to get sunstrings without recursion
+# Simple way to get substrings without recursion
 
 # def subStrings(string):
+#     print("")
 #     for i in range(len(string)):
 #         for j in range(i, len(string)):
 #             print(string[i:j+1])
