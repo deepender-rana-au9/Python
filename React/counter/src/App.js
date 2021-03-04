934f504bf8e158d809eb4c833df05ad860a896a5
Deepender Rana <chinturana97@gmail.com>
2021-03-04 12:13:08 +0530

    Todo App with context

--- React/counter/src/App.js
+++ React/counter/src/App.js
@@ -10,7 +10,7 @@ const App = () => {
       </header>
       <h2>Current value of count is {count}</h2>
       <button onClick={() => setCount(0)}>Reset Counter</button>
-      <button onClick={() => (count >= 10 ? "" : setCount(count + 1))}>
+      <button onClick={() => (count >= 20 ? "" : setCount(count + 1))}>
         Increase Counter
       </button>
       <button onClick={() => (count <= 0 ? "" : setCount(count - 1))}>
